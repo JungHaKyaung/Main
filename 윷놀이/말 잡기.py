ae871d25cb6b33ff837a0b010568455493d30163
ljg5489 <85863908+ljg5489@users.noreply.github.com>
2023-06-07 20:06:34 +0900

    Update 말 잡기.py

--- 윷놀이/말 잡기.py
+++ 윷놀이/말 잡기.py
@@ -7,7 +7,7 @@ def catching_a_horse(yut_py, yut_px, teams, current_team):
                 if (team["horses"][i]["y"] == yut_py) and (team["horses"][i]["x"] == yut_px):
                     catching_a_horse = (team["name"], i)
                     break
-            if catching_a_horse:
+            if catching_a_horse: 
                 break 
     return catching_a_horse
 
