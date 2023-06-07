17dc0d77f3da7b112bdec1cc39ce8f3b53a412fa
ljg5489 <85863908+ljg5489@users.noreply.github.com>
2023-06-07 22:23:31 +0900

    Update 윷놀이 판(이진규).py

--- 윷놀이/윷놀이 판(이진규).py
+++ 윷놀이/윷놀이 판(이진규).py
@@ -8,7 +8,7 @@ yut_board = [['-' for _ in range(7)] for _ in range(9)]
 # 판에 좌표 설정  
 for x in range(9):
     for y in range(7):  
-        yut_board[x][y] = f'({x}, {y})'
+        yut_board[x][y] = f'({x}, {y})' 
 
 # 이동 불가능한 좌표 설정  
 invalid_coordinates = [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(1,3),(7,3),(8,0),(8,1),(8,2),(8,3),(8,4),(8,5),(8,6),
