18f50e77ef367095a226257d1eff8b8cd1f3c06a
ljg5489 <85863908+ljg5489@users.noreply.github.com>
2023-06-08 23:54:06 +0900

    Update 말 잡기.py

--- 윷놀이/말 잡기.py
+++ 윷놀이/말 잡기.py
@@ -11,7 +11,7 @@ def catching_a_horse(yut_py, yut_px, teams, current_team):
                 break                      
     return catching_a_horse                   
                   
-                 
+                  
 #윷놀이 판 생성   
 yut_board = [['-' for _ in range(7)] for _ in range(9)]             
              
